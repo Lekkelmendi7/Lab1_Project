cdb1e166329d248c639edfa3fe0d37a302af7ce2
Bleronn <93322504+Bleronn@users.noreply.github.com>
2022-05-04 01:58:44 +0200

    Add files via upload

---
+++ Qyteti.cs
@@ -0,0 +1,9 @@
+﻿namespace lab1.Models
+{
+    public class Qyteti
+    {
+        public int QytetiId { get; set; }   
+        public string QytetiName { get; set; }  
+
+    }
+}
---
+++ labDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab1.Models
+{
+    public class labDbContext : DbContext
+    {
+        public labDbContext(DbContextOptions<labDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Qyteti> Qytetet { get; set; }
+    }
+}
