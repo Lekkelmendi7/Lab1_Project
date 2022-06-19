0e85b9e7fec98bba27577833526a35cf8acad24e
Bleronn <93322504+Bleronn@users.noreply.github.com>
2022-06-19 22:59:51 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
+global using Employee_Managment_System.Data;//
+using Employee_Managment_System.Services;//
+using Microsoft.EntityFrameworkCore;//
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+using System.Configuration;
+using Employee_Managment_System.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +17,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//for login(Auth) with hash password
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<JwtService>();
+
+//Connecting to SQL Server
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
